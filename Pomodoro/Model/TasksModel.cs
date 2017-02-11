2e3069b9add6d1a8e7a8389024e9391679d368bd
agangal <ashish.gangal1990@gmail.com>
2017-02-11 11:57:18 -0800

    model for task history

---
+++ Pomodoro/Model/TasksModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoro.Model
+{
+    public class TasksModel
+    {
+        public string task { get; set; }
+        public List<TaskHistory> histiry { get; set; }
+    }
+
+    public class TaskHistory
+    {
+        public TimeSpan timespan_completed { get; set; }
+        public timespan_type type { get; set; }
+    }
+
+    public enum timespan_type
+    {
+       pomodoro,
+       pomodoro_break
+    }
+}
